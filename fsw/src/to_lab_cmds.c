7ee490ee10e7aca569ffaee14b3c165b8f03ad8f
Simon Beck <simon.beck@ateleris.ch>
2026-05-21 16:03:25 +0200

    remove unused apqs MIDs

--- fsw/src/to_lab_cmds.c
+++ fsw/src/to_lab_cmds.c
@@ -50,7 +50,6 @@ static const CFE_SB_MsgId_Atom_t TO_LAB_HkMids[] = {
     CFE_TBL_HK_TLM_MID,
     CFE_TIME_HK_TLM_MID,
     CI_LAB_HK_TLM_MID,
-    APQS_APP_HK_TLM_MID,
     CFE_EVS_SHORT_EVENT_MSG_MID,
     CFE_EVS_LONG_EVENT_MSG_MID,
 };
